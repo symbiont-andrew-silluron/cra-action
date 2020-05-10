d3d9341f764c620e74fcc3b792208618f8c60211
symbiont-andrew-silluron <61030802+symbiont-andrew-silluron@users.noreply.github.com>
2020-05-09 18:48:35 -0600

    strip down for cwd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
 # Exit if any subcommand fails
-set -e 
-
-
-# Setup node modules if needed
-if [ -e /github/workspace/web/node_modules/.bin/jest ]; then
-    setup=""
-else
-    echo "## Installing node_modules..."
-    if [ -f yarn.lock ]; then
-        setup="yarn --non-interactive --silent --ignore-scripts --production=false &&"
-    else
-        setup="NODE_ENV=development npm install &&"
-    fi
-fi
 
 
 echo "## Running CRA"
