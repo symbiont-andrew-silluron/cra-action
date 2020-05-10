401e4dbc971f4545bd74294655d8268ce6e6c5dd
symbiont-andrew-silluron <61030802+symbiont-andrew-silluron@users.noreply.github.com>
2020-05-09 18:59:22 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
 # Exit if any subcommand fails
+set -e 
+
+
+# Setup node modules if needed
+if [ -e /github/workspace/web/node_modules/.bin/jest ]; then
+    setup=""
+else
+    echo "## Installing node_modules..."
+    if [ -f yarn.lock ]; then
+        setup="yarn --non-interactive --silent --ignore-scripts --production=false &&"
+    else
+        setup="NODE_ENV=development npm install &&"
+    fi
+fi
 
 
 echo "## Running CRA"
-pwd
-ls -a
-# sh -c "$setup ./node_modules/.bin/react-scripts test $args"
+args=$@
+cd /github/workspace/web
+sh -c "$setup ./node_modules/.bin/react-scripts test $args"
